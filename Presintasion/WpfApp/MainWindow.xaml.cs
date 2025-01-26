ea0821ea32ff14676f8e5a14e681c0d1df7a81e2
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-01-26 12:14:23 +0300

    add page autorisution

--- Presintasion/WpfApp/MainWindow.xaml.cs
+++ Presintasion/WpfApp/MainWindow.xaml.cs
@@ -25,39 +25,10 @@ namespace WpfApp
         public MainWindow(DataServis dataServis)
         {
             InitializeComponent();
+            //MainFrame.
             Loaded += MainWindow_Loaded;
             db.Database.EnsureCreated();
-
-            List<Company> сompanies =
-            [
-                new Company()
-                {
-                    LongName = "имяДлинное1",
-                    ShortName = "имяКороткое1",
-                    Description = "Описание1",
-                    AddresCompany = new Address(),
-                    Telephones = new List<Telephone>(),
-                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail1@mail.ru" } }
-                },
-                new Company()
-                {
-                    LongName = "имяДлинное2",
-                    ShortName = "имяКороткое2",
-                    Description = "Описание2",
-                    AddresCompany = new Address(),
-                    Telephones = new List<Telephone>(),
-                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail2@mail.ru" } }
-                },
-                new Company()
-                {
-                    LongName = "имяДлинное3",
-                    ShortName = "имяКороткое3",
-                    Description = "Описание3",
-                    AddresCompany = new Address(),
-                    Telephones = new List<Telephone>(),
-                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail3@mail.ru" } }
-                }
-            ];
+            List<Company> сompanies = WorkClass.GetContentBD();
 
             db.AddRange(сompanies.ToArray());
             db.SaveChanges();
---
+++ Presintasion/WpfApp/WorkClass.cs
@@ -0,0 +1,43 @@
+﻿using SoftServis;
+
+namespace WpfApp
+{
+    internal class WorkClass
+    {
+        internal static List<Company> GetContentBD()
+        {
+            List<Company> сompanies =
+            [
+                new Company()
+                {
+                    LongName = "имяДлинное1",
+                    ShortName = "имяКороткое1",
+                    Description = "Описание1",
+                    AddresCompany = new Address(),
+                    Telephones = new List<Telephone>(),
+                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail1@mail.ru" } }
+                },
+                new Company()
+                {
+                    LongName = "имяДлинное2",
+                    ShortName = "имяКороткое2",
+                    Description = "Описание2",
+                    AddresCompany = new Address(),
+                    Telephones = new List<Telephone>(),
+                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail2@mail.ru" } }
+                },
+                new Company()
+                {
+                    LongName = "имяДлинное3",
+                    ShortName = "имяКороткое3",
+                    Description = "Описание3",
+                    AddresCompany = new Address(),
+                    Telephones = new List<Telephone>(),
+                    Mailes = new List<Mail>(){ new Mail() {MailName = "Mail3@mail.ru" } }
+                }
+            ];
+
+            return сompanies;
+        }
+    }
+}
