c9d8b7634871b44172755fcfd9d0a464b4f833ff
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-02-24 13:09:01 +0300

    Add save canges on form commpany

--- Presintasion/WpfApp/WindowCompany.xaml.cs
+++ Presintasion/WpfApp/WindowCompany.xaml.cs
@@ -1,4 +1,6 @@
-﻿using SoftServis;
+﻿using Microsoft.Data.SqlClient;
+using SoftServis;
+using SoftServis.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,19 +22,51 @@ namespace WpfApp
     /// </summary>
     public partial class WindowCompany : Window
     {
+        Company CompanyCurrent {  get; set; }
         public WindowCompany(Company company)
         {
             InitializeComponent();
             FullName.Text = company.LongName;
             ShortName.Text = company.ShortName;
-            Mails.Text = company.Mailes.First().MailName;
+            Mails.Text = company.Mailes.First()?.MailName;
             INN.Text =  company.Id.ToString();
+            CompanyCurrent = company;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var text = HistoriBox.Text;
-            HistoriBlock.Text += text;
+            var date = DateTime.Now;
+            HistoriBlock.Text += $"{text}  {date} ";
+        }
+
+        private void Button_Save(object sender, RoutedEventArgs e)
+        {
+
+            using (ApplicationContext db = new())
+            {
+                var dd = db.companies.Where(x => x.Id == CompanyCurrent.Id).FirstOrDefault();
+                if (CompanyCurrent != null)
+                {
+                    dd.LongName = FullName.Text;
+                    dd.ShortName = ShortName.Text;
+                    dd.Id = int.Parse(INN.Text);
+                    dd.Mailes.Add(new Mail() { MailName = Mails.Text });
+
+                    db.SaveChanges();
+                }
+            }
+
+            //db.Database.
+            //string connectionString = @"Data Source=(localdb)\mssqllocaldb; Database=dbCompany; Trusted_Connection=True;";
+            //string sqlExpression = $"UPDATE сompanies SET Age={20} WHERE ShortName = {CompanyCurrent.ShortName}";
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //    SqlCommand command = new SqlCommand(sqlExpression, connection);
+            //    int number = command.ExecuteNonQuery();
+            //    Console.WriteLine("Удалено объектов: {0}", number);
+            //}
         }
     }
 }
